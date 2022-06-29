8ec5150d230470f9cb6e5ea4aeb23a793c6ae621
chr <1137528068@qq.com>
2022-06-29 15:29:24 +0800

    commit

---
+++ eight-digital-issues.py
@@ -0,0 +1,211 @@
+import numpy as np
+import time
+
+
+class State:
+    def __init__(self, state, answer, directionFlag=None, parent=None):
+        self.state = state
+        self.answer = answer
+        self.direction = ['up', 'down', 'right', 'left']
+        if directionFlag:
+            self.direction.remove(directionFlag)
+        self.parent = parent
+        self.getFunctionValue()
+
+    def getDirection(self):
+        return self.direction
+
+    def setF(self, f):
+        self.f = f
+        return
+
+    # 打印结果
+    def showInfo(self):
+        for i in range(len(self.state)):
+            for j in range(len(self.state)):
+                print(self.state[i, j], end='  ')
+            print("\n")
+        print('->')
+        return
+
+    # 获取0点
+    def getZeroPos(self):
+        postion = np.where(self.state == 0)
+        return postion
+
+    # g为深度
+    def getG(self):
+        if (self.parent is not None):
+            self.g = self.parent.g + 1
+        else:
+            self.g = 0
+
+    # h为到目标状态的距离，使用曼哈顿距离之和或不在位数计算
+    def getH(self, flag=True):
+        cur_node = self.state.copy()
+        fin_node = self.answer.copy()
+        dist = 0
+        N = len(cur_node)
+
+        if flag:  # 曼哈顿距离
+            for i in range(N):
+                for j in range(N):
+                    if cur_node[i][j] != fin_node[i][j]:
+                        index = np.argwhere(fin_node == cur_node[i][j])
+                        x = index[0][0]  # 最终x距离
+                        y = index[0][1]  # 最终y距离
+                        dist += (abs(x - i) + abs(y - j))
+        else:     # 不在位数
+            for i in range(N):
+                for j in range(N):
+                    if cur_node[i][j] != fin_node[i][j]:
+                        dist += 1
+
+        self.h = dist
+
+    # 启发函数 f = g + h
+    def getFunctionValue(self):
+        self.getG()
+        self.getH()
+        self.f = self.g + self.h
+
+        return self.f
+
+    def nextStep(self, generate):
+        if not self.direction:
+            return []  # direction为空时返回空列表
+        subStates = []
+        boarder = len(self.state) - 1
+        # 获取0点位置
+        x, y = self.getZeroPos()
+        # 向左
+        if 'left' in self.direction and y > 0:
+            s = self.state.copy()
+            a = self.answer.copy()
+            tmp = s[x, y - 1]
+            s[x, y - 1] = s[x, y]
+            s[x, y] = tmp
+            news = State(s, a, directionFlag='right', parent=self)
+            news.setF(news.getFunctionValue())
+            subStates.append(news)
+            generate[0] += 1
+        # 向上
+        if 'up' in self.direction and x > 0:
+            # it can move to upper place
+            s = self.state.copy()
+            a = self.answer.copy()
+            tmp = s[x - 1, y]
+            s[x - 1, y] = s[x, y]
+            s[x, y] = tmp
+            news = State(s, a, directionFlag='down', parent=self)
+            news.setF(news.getFunctionValue())
+            subStates.append(news)
+            generate[0] += 1
+        # 向下
+        if 'down' in self.direction and x < boarder:
+            # it can move to down place
+            s = self.state.copy()
+            a = self.answer.copy()
+            tmp = s[x + 1, y]
+            s[x + 1, y] = s[x, y]
+            s[x, y] = tmp
+            news = State(s, a, directionFlag='up', parent=self)
+            news.setF(news.getFunctionValue())
+            subStates.append(news)
+            generate[0] += 1
+        # 向右
+        if self.direction.count('right') and y < boarder:
+            # it can move to right place
+            s = self.state.copy()
+            a = self.answer.copy()
+            tmp = s[x, y + 1]
+            s[x, y + 1] = s[x, y]
+            s[x, y] = tmp
+            news = State(s, a, directionFlag='left', parent=self)
+            news.setF(news.getFunctionValue())
+            subStates.append(news)
+            generate[0] += 1
+        # 返回F值最小的下一个点
+        subStates.sort(key=lambda state: state.f)
+        return subStates
+
+    # A* 迭代
+    def solve(self, generate):
+        # openList
+        openTable = []
+        # closeList
+        closeTable = []
+
+        openTable.append(self)
+        while openTable:
+            # 对openTable表按f值升序排列
+            openTable.sort(key=lambda S: S.f)
+            # 下一步的点移除open
+            n = openTable.pop(0)
+            # 加入close
+            closeTable.append(n)
+            path = []
+            # 判断是否和最终结果相同
+            if (n.state == n.answer).all():
+                while n.parent and n.parent != originState:
+                    path.append(n.parent)
+                    n = n.parent
+                path.reverse()
+                return path
+
+            # 扩展结点
+            subStates = n.nextStep(generate)
+            for stat in subStates:
+                # 查看扩展出的状态是否已经存在于openTable或closeTable中
+                findstat = isin(stat, openTable)
+                findstat2 = isin(stat, closeTable)
+                # 在closeTable中,判断是否更新
+                if (findstat2[0] and stat.f < closeTable[findstat2[1]].f):
+                    closeTable[findstat2[1]] = stat
+                    openTable.append(stat)
+                # 在openTable中，判断是否更新
+                if (findstat[0] and stat.f < openTable[findstat[1]].f):
+                    openTable[findstat[1]] = stat
+                # stat状态不在openTable中，也不在closeTable中
+                if (not findstat[0] and not findstat2[0]):
+                    openTable.append(stat)
+        else:
+            return None, None  # 循环最后输出空值
+
+
+# 判断状态s是否在状态集合中，s是对象，sList是对象列表
+# 返回的结果是一个列表，第一个值是真假，如果是真则第二个值是g在gList中的位置索引
+def isin(s, sList):
+    state = s.state.copy()
+    state = state.tolist()
+    statList = []
+    for i in sList:
+        sta = i.state.copy()
+        sta = sta.tolist()
+        statList.append(sta)
+    if(state in statList):
+        res = [True, statList.index(state)]
+    else:
+        res = [False, 0]
+    return res
+
+
+if __name__ == '__main__':
+    start = time.clock()
+
+    originState = State(np.array([[2, 0, 8], [1, 6, 3], [7, 5, 4]]),
+                        np.array([[1, 2, 3], [8, 0, 4], [7, 6, 5]]))
+
+    s1 = State(state=originState.state,
+               answer=originState.answer)
+    generate = [0]
+    path = s1.solve(generate)
+    if path:
+        for node in path:
+            node.showInfo()
+        print(s1.answer)
+        print("Total steps is %d" % len(path))
+        print(f'generate node: {generate[0]}')
+
+    end = time.clock()
+    print(f"time is {end - start}")
---
+++ maze-pathfinding.py
@@ -0,0 +1,188 @@
+import numpy as np
+import time
+
+
+class Point:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+        self.f = 0
+
+    def setF(self, f):
+        self.f = f
+
+    def __eq__(self, other):
+        return self.x == other.x and self.y == other.y
+
+    # g为到起点最短路径代价的估计值
+    def getG(self, start):
+        dist = abs(self.x - start.x) + abs(self.y - start.y)
+        self.g = dist
+
+    # h为到终点最短路径代价的估计值，使用曼哈顿距离或欧式距离计算
+    def getH(self, end, flag=False):
+        if flag:  # 曼哈顿距离
+            dist = abs(self.x - end.x) + abs(self.y - end.y)
+            dist *= 10
+        else:     # 欧式距离
+            dist = pow(self.x - end.x, 2) + pow(self.y - end.y, 2)
+
+        self.h = dist
+
+    # 启发函数 f = g + h
+    def getFunctionValue(self, start, end):
+        self.getG(start)
+        self.getH(end)
+        self.f = self.g + self.h
+
+        return self.f
+
+
+class State:
+    def __init__(self, state, current_point=Point(0, 0), start_point=Point(0, 0), end_point=Point(0, 0)):
+        self.state = state
+        self.cP = current_point
+        self.sP = start_point
+        self.eP = end_point
+
+    def __eq__(self, other):
+        return self.cP == other.cP
+
+    def setF(self, f):
+        self.f = f
+
+    def setCurrentPoint(self, x, y):
+        self.cP.x = x
+        self.cP.y = y
+
+    def getCurPoint(self):
+        return self.cP.x, self.cP.y
+
+
+# 确定下一步的方法
+def nextStep(map, openTable, closeTable, wrongTable, generate):
+    subPoints = []
+    boarder = len(map.state) - 1
+    # 获取当前所在的点
+    x, y = map.getCurPoint()
+    # 往左走
+    if y > 0 and map.state[x][y - 1] == 0:
+        p = Point(x, y - 1)
+        if p not in closeTable and p not in wrongTable:
+            # 添加到可以走的list
+            openTable.append(p)
+            # new point
+            # 获取F函数值
+            p.setF(p.getFunctionValue(map.sP, map.eP))
+            subPoints.append(p)
+            generate[0] += 1
+    # 往上走
+    if x > 0 and map.state[x - 1][y] == 0:
+        p = Point(x - 1, y)
+        if p not in closeTable and p not in wrongTable:
+            openTable.append(p)
+            p.setF(p.getFunctionValue(map.sP, map.eP))
+            subPoints.append(p)
+            generate[0] += 1
+    # 往下走
+    if x < boarder and map.state[x + 1][y] == 0:
+        p = Point(x + 1, y)
+        if p not in closeTable and p not in wrongTable:
+            openTable.append(p)
+            p.setF(p.getFunctionValue(map.sP, map.eP))
+            subPoints.append(p)
+            generate[0] += 1
+    # 往右走
+    if y < boarder and map.state[x][y + 1] == 0:
+        p = Point(x, y + 1)
+        if p not in closeTable and p not in wrongTable:
+            openTable.append(p)
+            p.setF(p.getFunctionValue(map.sP, map.eP))
+            subPoints.append(p)
+            generate[0] += 1
+    # 根据F值排序，获取F值最近的
+    subPoints.sort(key=compareF)
+    if len(subPoints) < 1:
+        # 防止走到死路无法回头情况
+        wrongTable.append(Point(map.cP.x, map.cP.y))
+        closeTable.remove(map.cP)
+        next_point = closeTable[-1]
+        map.cP.x, map.cP.y = next_point.x, next_point.y
+    else:
+        next_point = subPoints[0]
+        map.cP.x, map.cP.y = next_point.x, next_point.y
+        closeTable.append(next_point)
+        openTable.remove(next_point)
+
+
+# 迭代走下一步
+def solve(map, openTable, closeTable, wrongTable, generate):
+    # start the loop
+    while not map.cP == map.eP:
+        nextStep(map, openTable, closeTable, wrongTable, generate)
+
+
+def compareF(p):
+    return p.f
+
+
+# 展示最后结果
+def showInfo(map, path):
+    for i in range(len(map.state)):
+        for j in range(len(map.state)):
+            if Point(i, j) in path:
+                # 正确路径用‘*’表示
+                print('*', end='  ')
+            else:
+                print(map.state[i, j], end='  ')
+        print("\n")
+    return
+
+
+if __name__ == '__main__':
+    start = time.clock()
+
+    # openList
+    openTable = []
+    # closeList
+    closeTable = []
+    # 走错路返回用的
+    wrongTable = []
+    state = np.array([[0, 0, 0, 0, 0], [1, 0, 1, 0, 1], [0, 0, 0, 0, 1], [0, 1, 0, 0, 0], [0, 0, 0, 1, 0]])
+    '''
+    state = np.array([[0, 1, 0, 1, 0, 1, 1, 0, 1, 1, 0, 0, 1, 1, 1, 0, 1, 1, 1, 1],
+                      [0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 1, 0, 1, 0, 0, 0, 1, 1],
+                      [0, 1, 0, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 1, 1, 0, 1, 1],
+                      [0, 0, 0, 1, 0, 0, 1, 0, 0, 0, 1, 0, 1, 0, 0, 0, 1, 0, 1, 1],
+                      [1, 1, 1, 1, 0, 1, 0, 0, 1, 1, 1, 0, 0, 1, 1, 1, 0, 0, 1, 1],
+                      [0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 1, 1, 1, 1, 0, 1, 1, 0, 0, 1],
+                      [0, 1, 0, 1, 0, 0, 0, 0, 1, 0, 1, 0, 0, 1, 0, 0, 1, 0, 1, 1],
+                      [1, 0, 0, 0, 0, 1, 0, 0, 0, 1, 1, 0, 0, 1, 1, 1, 1, 0, 0, 1],
+                      [0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 1, 1, 0, 1],
+                      [0, 1, 0, 0, 1, 1, 1, 1, 1, 1, 0, 0, 0, 1, 0, 1, 1, 0, 1, 0],
+                      [0, 0, 1, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 0, 0, 0, 1],
+                      [0, 1, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 1, 1],
+                      [0, 1, 1, 1, 0, 1, 0, 1, 0, 0, 1, 1, 1, 0, 0, 1, 0, 0, 0, 0],
+                      [0, 0, 0, 0, 0, 1, 0, 1, 1, 1, 0, 1, 1, 1, 1, 1, 0, 0, 0, 1],
+                      [1, 1, 0, 1, 1, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+                      [0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+                      [0, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0],
+                      [0, 1, 0, 0, 1, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                      [1, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0, 1, 1, 0],
+                      [0, 0, 1, 1, 1, 1, 0, 0, 1, 1, 1, 1, 1, 0, 0, 1, 1, 0, 1, 0]])'''
+    # 起点终点
+    start_point = Point(0, 0)
+    end_point = Point(4, 4)
+    # 最终路径
+    generate = [0]
+    path = [start_point]
+    Map = State(state, Point(0, 0), start_point, end_point)
+    solve(Map, openTable, closeTable, wrongTable, generate)
+    print('Best Way:')
+    path = path + closeTable
+    showInfo(Map, path)
+    print("Total steps is %d" % (len(path) - 1))
+    print(f'generate node: {generate[0]}')
+
+    end = time.clock()
+    print(f"time is {end - start}")
